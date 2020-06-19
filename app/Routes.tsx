0a2463b68bb51e151be82f12df3be2bff2c8cc3d
rikim <rikim@riotgames.com>
2020-06-19 22:34:04 +0900

    Add Router /consumer

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
-import ClusterOverview from "./components/ClusterOverview";
-import TopicDetail from "./components/TopicDetail";
-import ConsumerGroupDetail from "./components/ConsumerGroupDetail";
-import ConnectionOverview from "./components/ConnectionOverview";
+import ClusterOverview from './components/ClusterOverview';
+import TopicDetail from './components/TopicDetail';
+import ConsumerGroupDetail from './components/ConsumerGroupDetail';
+import ConnectionOverview from './components/ConnectionOverview';
+import Consumer from './components/Consumer';
 
 export default function Routes() {
   return (
@@ -16,7 +16,8 @@ export default function Routes() {
         <Route path={routes.COUNTER} component={ConnectionOverview} />
         <Route path={routes.CLUSTER_OVERVIEW} component={ClusterOverview} />
         <Route path={routes.TOPIC_DETAIL} component={TopicDetail} />
-        <Route path={'/consumer-group-detail'} component={ConsumerGroupDetail} />
+        <Route path={routes.CONSUMER_GROUP} component={ConsumerGroupDetail} />
+        <Route path={routes.CONSUMER} component={Consumer} />
         <Route path={routes.HOME} component={HomePage} />
       </Switch>
     </App>
