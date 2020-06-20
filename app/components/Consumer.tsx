2c91e2c6f9a46aceacd5bd2d9aec7f43f31bbc5a
rikim <rikim@riotgames.com>
2020-06-20 17:39:27 +0900

    Add stopConsume action in componentWillUnmount

--- app/components/Consumer.tsx
+++ app/components/Consumer.tsx
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators, Dispatch} from 'redux';
 import {ITopicMetadata, Kafka} from 'kafkajs';
-import {consumeTopic} from "../actions/consumer.actions";
+import {consumeTopic, stopConsume} from "../actions/consumer.actions";
+import './consumer.styles.scss'
 
 type Props = {
   topics: ITopicMetadata[],
   location: any,
   kafkaClient: Kafka,
   consumeTopic: (topic: string, groupId: string) => void,
+  stopConsume: () => void,
   consumer: any
 };
 
@@ -21,7 +23,8 @@ function mapStateToProps(state: any) {
 }
 function mapDispatchToProps(dispatch: Dispatch) {
   return bindActionCreators({
-    consumeTopic
+    consumeTopic,
+    stopConsume
   }, dispatch);
 }
 
@@ -32,10 +35,15 @@ export class Consumer extends Component<Props> {
     consumeTopic(location.search.split("=")[1], "group-jafka");
   }
 
+  componentWillUnmount(): void {
+    this.props.stopConsume();
+  }
+
   render(): JSX.Element {
-    const {location, consumer} = this.props;
+    const {location, history, consumer} = this.props;
     return (
-      <div>
+      <div className='consumer'>
+        <button onClick={() => history.goBack()}>Back</button>
         <h1>{location.search.split('=')[1]}</h1>
         <div>
           <select name="topic" defaultValue="">
@@ -44,7 +52,7 @@ export class Consumer extends Component<Props> {
           <div>{consumer.topic}</div>
           <ul key={consumer.topic}>
             {
-              consumer.message.map(msg => <li key={`${msg.value.sessionToken}-${msg.value.time}`}>{msg.key}</li>)
+              consumer.message.map(msg => <li key={`${msg.value.sessionToken}-${msg.value.time}`}>{JSON.stringify(msg.value)}</li>)
             }
           </ul>
         </div>
