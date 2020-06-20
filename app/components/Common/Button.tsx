aaafa0b21938b58d5e3bcf00b35472978b86db0c
rikim <rikim@riotgames.com>
2020-06-21 01:13:04 +0900

    Add stopPropagation onClick of Button.tsx

--- app/components/Common/Button.tsx
+++ app/components/Common/Button.tsx
@@ -21,6 +21,9 @@ interface Medium {
 export function Button(props: Props) {
   const {text, onClick, theme} = props;
   return (
-    <span onClick={() => onClick()} className={theme === 'small' ? styles.smallBtn : styles.mediumBtn}><i>[ {text} ]</i></span>
+    <span onClick={e => {
+      e.stopPropagation();
+      onClick();
+    }} className={theme === 'small' ? styles.smallBtn : styles.mediumBtn}><i>[ {text} ]</i></span>
   );
 }
