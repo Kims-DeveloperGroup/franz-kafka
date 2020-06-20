1250d26cf52df0200448f86dfc99a2eda4b5bbbe
rikim <rikim@riotgames.com>
2020-06-20 19:49:24 +0900

    Add start and stop function of consuming topic

--- app/components/Consumer.tsx
+++ app/components/Consumer.tsx
@@ -3,7 +3,8 @@ import {connect} from 'react-redux';
 import {bindActionCreators, Dispatch} from 'redux';
 import {ITopicMetadata, Kafka} from 'kafkajs';
 import {consumeTopic, stopConsume} from "../actions/consumer.actions";
-import './consumer.styles.scss'
+import './consumer.styles.scss';
+import {getTopicDetails} from "../actions/topic.detail.actions";
 
 type Props = {
   topics: ITopicMetadata[],
@@ -11,12 +12,14 @@ type Props = {
   kafkaClient: Kafka,
   consumeTopic: (topic: string, groupId: string) => void,
   stopConsume: () => void,
+  getTopicDetails: (topic) => void,
   consumer: any
 };
 
 function mapStateToProps(state: any) {
   return {
     topics: state.topics,
+    topicDetail: state.topicDetail,
     kafkaClient: state.kafka.client,
     consumer: state.consumer
   };
@@ -24,32 +27,42 @@ function mapStateToProps(state: any) {
 function mapDispatchToProps(dispatch: Dispatch) {
   return bindActionCreators({
     consumeTopic,
-    stopConsume
+    stopConsume,
+    getTopicDetails
   }, dispatch);
 }
 
 export class Consumer extends Component<Props> {
   componentDidMount(): void {
-    const {location, consumeTopic} = this.props;
-    console.log(location.search.split("=")[1]);
-    consumeTopic(location.search.split("=")[1], "group-jafka");
   }
 
   componentWillUnmount(): void {
     this.props.stopConsume();
   }
 
+  startConsume(): void {
+    const {consumeTopic, getTopicDetails} = this.props;
+    let topic = this.selectedTopic.value;
+    consumeTopic(topic, "group-jafka");
+    getTopicDetails(topic);
+  }
+
   render(): JSX.Element {
-    const {location, history, consumer} = this.props;
+    const {location, stopConsume, history, consumer, topicDetail, topics} = this.props;
     return (
       <div className='consumer'>
         <button onClick={() => history.goBack()}>Back</button>
-        <h1>{location.search.split('=')[1]}</h1>
+        <h1>{consumer.topic ? `Consuming ${consumer.topic}` : 'Consumer'}</h1>
         <div>
-          <select name="topic" defaultValue="">
+          <select name="topic" defaultValue={location.search.split("=")[1]}
+                  ref={e => this.selectedTopic = e}
+                  onChange={() => stopConsume()}>
             <option value="">Select topic</option>
+            {
+              topics.map(topic => <option key={topic.name} value={topic.name}>{topic.name}</option>)
+            }
           </select>
-          <div>{consumer.topic}</div>
+          <button onClick={() => consumer.topic ? stopConsume() : this.startConsume()}>{consumer.topic ? "Stop" : "Start"}</button>
           <ul key={consumer.topic} className='messages'>
             {
               consumer.message.map(msg => <li key={`${msg.offset}-${msg.partition}`}>partition: {msg.partition}: {msg.offset}<br/> message: {JSON.stringify(msg.value)}</li>)
