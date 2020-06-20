9d4effbdef71f3c84ab94c34e8dae0966d2f673a
rikim <rikim@riotgames.com>
2020-06-20 17:31:14 +0900

    Add STOP_CONSUME action and move dispatching consumeStart action after connection

--- app/reducers/consumer.reducer.ts
+++ app/reducers/consumer.reducer.ts
@@ -1,17 +1,29 @@
 import {AnyAction} from 'redux';
-import {CONSUME_MESSAGE, START_CONSUME} from "../actions/consumer.actions";
+import {CONSUME_MESSAGE, START_CONSUME, STOP_CONSUME} from "../actions/consumer.actions";
 
-export default function consumer(state: any = {topic: '', message: []}, action: AnyAction) {
+const MAX_MESSAGE_COUNT = 10;
+
+let emptyConsumer = {consumer: null, topic: '', message: []};
+export default function consumer(state: any = emptyConsumer, action: AnyAction) {
   const newState = { ...state };
   switch (action.type) {
     case START_CONSUME:
+      newState.consumer = action.consumer;
       newState.topic = action.topic;
       newState.message = [];
       return newState;
     case CONSUME_MESSAGE:
       action.message.value = JSON.parse(action.message.value);
+      if (newState.message.length > MAX_MESSAGE_COUNT) {
+        newState.message = newState.message.slice(newState.message.length - MAX_MESSAGE_COUNT);
+      }
       newState.message.push(action.message);
       return newState;
+    case STOP_CONSUME:
+      if (state.consumer) {
+        state.consumer.pause();
+      }
+      return emptyConsumer;
     default:
       return state;
   }
