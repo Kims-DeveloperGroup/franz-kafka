320a7f1d720364d169b250753cc638867371af41
rikim <rikim@riotgames.com>
2020-06-21 14:03:37 +0900

    Add app name in Home.tsx

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
 export default function Home() {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
+      <h2>Jafka</h2>
       <Link to={routes.CONNECTIONS}><i>[ to connections ]</i></Link>
     </div>
   );
