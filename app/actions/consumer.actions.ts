c4df9d538ef972a44276aa61478e0cfbd892da9b
rikim <rikim@riotgames.com>
2020-06-22 17:30:15 +0900

    Add text search input

--- app/actions/consumer.actions.ts
+++ app/actions/consumer.actions.ts
@@ -30,22 +30,30 @@ export function stopConsume(flushMsg:boolean = true) {
   return (dispatch: Dispatch) => dispatch(consumerStop(flushMsg))
 }
 
-export function consumeTopic(topicToConsume, groupId, fromBeginning = false) {
+export function consumeTopic(topicToConsume, groupId, fromBeginning = false, regexLiteral = '') {
   return (dispatch: Dispatch, getState: GetState) => {
     let consumer = getState().kafka.client.consumer({ groupId: groupId });
+    const regex = new RegExp(regexLiteral);
     consumer.connect()
       .then(async () => {
         dispatch(consumerStart(consumer, topicToConsume));
         await consumer.subscribe({ topic: topicToConsume, fromBeginning: fromBeginning });
         await consumer.run({
           eachMessage: async ({ topic, partition, message }) => {
+            const value = message.value.toString();
+            let matched = false;
+            if (regexLiteral) {
+              matched = value.match(regex) !== null
+            }
+
             dispatch(messageConsume({
               key: message.key.toString(),
-              value: message.value.toString(),
+              value: value,
               offset: message.offset,
               timeStamp: message.timestamp,
               topic: topic,
-              partition: partition
+              partition: partition,
+              matched: matched
             }))
           },
         })
--- app/components/Consumer.tsx
+++ app/components/Consumer.tsx
@@ -7,12 +7,13 @@ import './consumer.styles.scss';
 import {getTopicDetails} from "../actions/topic.detail.actions";
 import {History} from 'history';
 import {Button} from "./Common/Button";
+import {TextInput} from "./Common/TextInput";
 
 type Props = {
   topics: ITopicMetadata[],
   location: any,
   kafkaClient: Kafka,
-  consumeTopic: (topic: string, groupId: string, fromBeginning: boolean) => void,
+  consumeTopic: (topic: string, groupId: string, fromBeginning: boolean, regex: string) => void,
   stopConsume: (flushMsg:boolean) => void,
   getTopicDetails: (topic: string) => void,
   consumer: any,
@@ -51,7 +52,7 @@ export class Consumer extends Component<Props> {
   startConsume(fromBeginning: boolean): void {
     const {consumeTopic, getTopicDetails} = this.props;
     let topic = this.selectedTopic.value;
-    consumeTopic(topic, "group-jafka", fromBeginning);
+    consumeTopic(topic, "group-jafka", fromBeginning, this.matchText.value);
     getTopicDetails(topic);
   }
 
@@ -87,11 +88,12 @@ export class Consumer extends Component<Props> {
                 <Button text="Start from beginning" theme='small' onClick={() => this.startConsume(true)}/>
               </span>
           }
-
         </div>
         <br/>
+        {!consumer.topic && <TextInput refer={e => (this.matchText = e)} placeholder='(Optional)Text to search'/>}
+        <br/><br/>
         <div>
-          <h3 className='colored-2'>{consumer.message.length ? `partition-${consumer.message[consumer.message.length - 1].partition} : ${consumer.message[consumer.message.length - 1].offset} : ${consumer.message[consumer.message.length - 1].timeStamp} : ${consumer.message[consumer.message.length - 1].key}`: 'NONE'}</h3>
+          <h3 className='colored-2'>{consumer.message.length ? `partition-${consumer.message[consumer.message.length - 1].partition} : ${consumer.message[consumer.message.length - 1].offset} : ${consumer.message[consumer.message.length - 1].timeStamp} : ${consumer.message[consumer.message.length - 1].key}`: ''}</h3>
         </div>
         <ul key={consumer.topic} className='messages ul-40'>
           {
@@ -105,6 +107,24 @@ export class Consumer extends Component<Props> {
           }
           <li className='colored' ref={el => this.messagesEnd = el}>-----------EOL-----------</li>
         </ul>
+        {
+          consumer.matched.length > 0 &&
+          <div>
+            <h3>Matched : {this.matchText ? this.matchText.value : ''}</h3>
+            <ul key={`search-${consumer.topic}`} className='messages ul-40'>
+              {
+                consumer.matched.map(msg => <li className='message' key={`${msg.offset}-${msg.partition}`}>
+                  <div>
+                    <h3 className='colored'>partition-{msg.partition}: {msg.offset}</h3><br/>
+                    <div className='highlight'>&lt;timestamp&gt;: <span>{msg.timeStamp}</span></div>
+                    <div className='highlight'>&lt;message&gt;:<br/> {JSON.stringify(msg.value)}</div>
+                  </div>
+                </li>)
+              }
+              <li className='colored'>-----------EOL-----------</li>
+            </ul>
+          </div>
+        }
       </div>
     );
   }
