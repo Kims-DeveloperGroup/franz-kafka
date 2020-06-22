7271877be9469c4975e640a37d14cc0a806c7596
rikim <rikim@riotgames.com>
2020-06-22 15:14:52 +0900

    Rename Home.css to Home.scss

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import routes from '../constants/routes.json';
-import styles from './Home.css';
+import styles from './Home.scss';
 
 export default function Home() {
   return (
