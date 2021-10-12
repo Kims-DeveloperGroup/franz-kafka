5e6eea079546ce39e7d20949e6c28b8f95a5a0e7
rica.rdo <rica.rdo@kakaopaycorp.com>
2021-10-12 16:00:48 +0900

    Indicate the explict type of reason in catch block of connectKafkaCluster

--- app/components/ConnectionOverview.tsx
+++ app/components/ConnectionOverview.tsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {bindActionCreators, Dispatch} from 'redux';
 import {connect} from 'react-redux';
-import {Kafka} from 'kafkajs';
+import {Kafka, KafkaJSError} from 'kafkajs';
 import {connectKafkaCluster} from '../actions/kafka.actions';
 import routes from '../constants/routes.json';
 import * as _ from 'lodash';
@@ -31,6 +31,7 @@ function mapDispatchToProps(dispatch: Dispatch) {
 }
 
 export class ClusterOverview extends Component<Props> {
+  private url: string;
   constructor(props: Readonly<Props>) {
     super(props);
     this.url = '';
@@ -60,8 +61,8 @@ export class ClusterOverview extends Component<Props> {
     this.addRecentConnections(url);
     connectKafkaCluster(url)
       .then(() => history.push(routes.CLUSTER_OVERVIEW))
-      .catch(reason => {
-        alert(reason);
+      .catch((reason: KafkaJSError) => {
+        alert(reason.message);
       });
   }
 
@@ -70,19 +71,35 @@ export class ClusterOverview extends Component<Props> {
     return (
       <div>
         <div>
-          <Button text='Back Home' onClick={() => history.push(routes.HOME)} theme='medium'/>
+          <Button
+            text="Back Home"
+            onClick={() => history.push(routes.HOME)}
+            theme="medium"
+          />
         </div>
         <div>
           <h1>Connections</h1>
-          URL : <TextInput refer={e => (this.url = e)} placeholder='127.0.0.1:9092,127.0.0.1:9093'/>
-          <Button text='connect'
-                  theme='small'
-                  onClick={() => this.connectKafkaCluster(this.url.value)}/>
-          <ul className='ul-40'>
+          URL :
+          <TextInput
+            refer={e => {
+              this.url = e;
+            }}
+            placeholder="127.0.0.1:9092,127.0.0.1:9093"
+          />
+          <Button
+            text="connect"
+            theme="small"
+            onClick={() => this.connectKafkaCluster(this.url.value)}
+          />
+          <ul className="ul-40">
             {this.loadRecentConnection().map(url => (
               <li key={url}>
                 <span>{url}</span>
-                <Button text='reconnect' onClick={() => this.connectKafkaCluster(url)} theme='small'/>
+                <Button
+                  text="reconnect"
+                  onClick={() => this.connectKafkaCluster(url)}
+                  theme="small"
+                />
               </li>
             ))}
           </ul>
